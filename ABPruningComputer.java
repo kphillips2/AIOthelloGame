cc0bba12a2e4b8af7a9ce693cdc9fdf721ebb911
evanliferock <evanliferock@gmail.com>
2017-04-17 17:59:39 -0700

    Fixed a typo and added checking for no possible moves

--- ABPruningComputer.java
+++ ABPruningComputer.java
@@ -7,7 +7,7 @@ public class ABPruningComputer extends Player{
    int[] makeMove(Board b){
       int[] move = {-1, -1};
       char[][] board = b.getBoardArrayCopy();
-      double[] theReturn = recursiveDecesnt(board, move, Double.NEGATIVE_INFINITY, 
+      double[] theReturn = recursiveDescent(board, move, Double.NEGATIVE_INFINITY, 
                                          Double.POSITIVE_INFINITY, 0);
       move[0] = (int)theReturn[0];
       move[1] = (int)theReturn[1];
@@ -37,7 +37,7 @@ public class ABPruningComputer extends Player{
     * @param height      Contains the current height of the move being checked
     * @return the best possible move and the heuristic. So [row,column,heuristic]
     */
-   private double[] recursiveDecesnt(char[][] theBoard, int[] currentMove, double alpha, double beta,
+   private double[] recursiveDescent(char[][] theBoard, int[] currentMove, double alpha, double beta,
                           int height){
       char[][] newBoard = Board.copyBoard(theBoard);
       // gets whos turn it is
@@ -58,10 +58,24 @@ public class ABPruningComputer extends Player{
          returnMove[1] = currentMove[1];
          returnMove[2] = getHeuristic(newBoard);
          return returnMove;
-      }else if(height % 2 == 0){
-         
       }else{
+         ArrayList<int[]> possibleMoves = Board.getPossibleMoves(newBoard, currentPlayer);
+         ArrayList<double[]> returnedMoves = new ArrayList<double[]>();
          
+         if(possibleMoves.size() == 0){
+            // if simulated player is unable to move
+            int[] move = {-1, -1};
+            returnedMoves.add(recursiveDescent(newBoard, move, alpha, beta, height + 1));
+            
+         }else{
+            if(currentPlayer == colour){
+            // This is simulating the computers turn
+            
+            }else{
+            // This is simulating the humans turn
+            
+            }
+         }
       }
       // This is so it compiles
       returnMove[0] = currentMove[0];
